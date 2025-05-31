2e3086a0291f38adb2cd6d3490866aa8b38ad611
Oliver Schulz <oschulz@mpp.mpg.de>
2025-05-31 15:26:21 +0200

    Instantiate hep-base for all Julia versions

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,7 @@ COPY environments/hep-base/*.toml environments/hep-base/
 
 ENV JULIA_PKG_PRESERVE_TIERED_INSTALLED="true"
 
-RUN provisioning/install-julia-packages.sh environments/hep-base
+RUN true \
+    && provisioning/install-julia-packages.sh julia-1.11 environments/hep-base \
+    && provisioning/install-julia-packages.sh julia-1.12 environments/hep-base \
+    true
--- provisioning/install-julia-packages.sh
+++ provisioning/install-julia-packages.sh
@@ -1,10 +1,16 @@
 #/bin/bash
 set -e
 
-JL_REF_ENV="$1"
+JL_EXE="$1"
+JL_REF_ENV="$2"
 JL_ENV_NAME=`basename "$JL_REF_ENV"`
 
-export JULIA_DEPOT_PATH="/opt/julia/local/share/julia:" 
+JL_LOCAL_DIR=`julia -e 'println(first(filter(p -> !startswith(p, homedir()), DEPOT_PATH)))'`
+
+export JULIA_DEPOT_PATH="${JL_LOCAL_DIR}:"
+# Same real path as symlink:
+# export JULIA_DEPOT_PATH="/opt/julia-local/share/julia:"
+
 export JULIA_PKG_PRESERVE_TIERED_INSTALLED="true"
 
 DEFAULT_NUM_THREADS=`lscpu -p | grep '^[0-9]\+,[0-9]\+,[0-9]\+,0,' | cut -d ',' -f 2 | sort | uniq | wc -l`
@@ -13,21 +19,20 @@ export OPENBLAS_NUM_THREADS="${DEFAULT_NUM_THREADS}"
 export OMP_NUM_THREADS="${DEFAULT_NUM_THREADS}"
 export GKSwstype="nul"
 
-julia -e 'import Pkg; Pkg.Registry.add("General"); Pkg.Registry.add(url = "https://github.com/legend-exp/LegendJuliaRegistry.git")'
+"${JL_EXE}" -e 'import Pkg; Pkg.Registry.add("General"); Pkg.Registry.add(url = "https://github.com/legend-exp/LegendJuliaRegistry.git")'
 
-JL_ENVDIR=$(dirname $(dirname `julia -e 'import Pkg; println(Pkg.project().path)'`))
+JL_ENVDIR=$(dirname $(dirname `"${JL_EXE}" -e 'import Pkg; println(Pkg.project().path)'`))
 export JULIA_PROJECT="${JL_ENVDIR}/$JL_ENV_NAME"
 
 mkdir -p "${JULIA_PROJECT}"
 cp -a "$JL_REF_ENV"/*.toml "${JULIA_PROJECT}"/
 chown -R root:root "${JULIA_PROJECT}"
 
-julia -e 'import Pkg; Pkg.instantiate()'
+"${JL_EXE}" -e 'import Pkg; Pkg.instantiate()'
 
 unset JULIA_PROJECT
-export JULIA_PROJECT=$(dirname `julia -e 'import Pkg; println(Pkg.project().path)'`)
+export JULIA_PROJECT=$(dirname `"${JL_EXE}" -e 'import Pkg; println(Pkg.project().path)'`)
 mkdir -p "${JULIA_PROJECT}"
-julia -e 'import Pkg; Pkg.add(["Revise", "IJulia", "Pluto"]); Pkg.build("IJulia")'
-rm /opt/julia/local/share/julia/logs/manifest_usage.toml
-# rm -rf /opt/julia/local/share/julia/logs
-chmod -R go+rX  "/opt/julia/local/share/julia"
+"${JL_EXE}" -e 'import Pkg; Pkg.add(["Revise", "IJulia", "Pluto"]); Pkg.build("IJulia")'
+rm "${JL_LOCAL_DIR}/logs/manifest_usage.toml"
+chmod -R go+rX  "${JL_LOCAL_DIR}"
